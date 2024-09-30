aa52c8e545ff52c66e92a684004d6b2846bc7a38
David Bosschaert <bosschae@adobe.com>
2024-09-30 10:29:57 +0100

    Multi-platform docker image

--- sling-mini/build_docker.sh
+++ sling-mini/build_docker.sh
@@ -1,2 +1,2 @@
-docker build -f Dockerfile.offline  -t bosschaert/sling-mini-offline .
+docker build -f Dockerfile.offline  -t bosschaert/sling-mini-offline --platform linux/amd64,linux/arm64/v8 .
 
--- sling-mini/launch-docker.sh
+++ sling-mini/launch-docker.sh
@@ -1,3 +1,3 @@
 rm content/offline/*
-docker run -v /Users/david/clones/sling-whiteboard_2/sling-mini/content/docs:/tmp/docs -v /Users/david/clones/sling-whiteboard_2/sling-mini/content/offline:/tmp/offliner   bosschaert/sling-mini-offline
+docker run -v /Users/david/clones/sling-whiteboard_2/sling-mini/content/docs:/tmp/docs -v /Users/david/clones/sling-whiteboard_2/sling-mini/content/offline:/tmp/offliner bosschaert/sling-mini-offline:1
 
